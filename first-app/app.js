e92282c9bf7caba3b0866b77ece271a457e38bfa
nihaprezz <serg13prez@outlook.com>
2020-02-06 17:43:15 -0500

    testing node server

--- first-app/app.js
+++ first-app/app.js
@@ -15,5 +15,20 @@ console.log(freeMemory)
 //FILE SYSTEM
 const fs = require('fs');
 
+//Creating a quick server. 
+const http = require('http')  // require the http module
+const hostname = `127.0.0.1`
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at https://${hostname}:${port}`)
+})
+
 
 
