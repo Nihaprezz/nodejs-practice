6e1e77f89c0e84aea0246b72c858714d8b9079f2
nihaprezz <serg13prez@outlook.com>
2020-02-07 15:32:24 -0500

    test api inital set up

--- restful-api-test/routes/users.js
+++ restful-api-test/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [
+  { id: 1, name: 'sergio'},
+  { id: 2, name: 'billy'},
+  { id: 3, name: 'joe'},
+  { id: 4, name: 'dave'}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
+  console.log(users)
 });
 
 module.exports = router;
